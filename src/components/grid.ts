cc58c77e800b2b666658eff38c1b771b3319cf30
sulhadin <sulhadin@gmail.com>
2021-03-13 00:22:00 +0300

    feat: Implemented grid layout.
    * Added assets.

---
+++ src/components/grid.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+//
+// export const Grid = styled.div``
+//
+// export const Row = styled.div`
+//     display: flex;
+// `
+//
+export const Col = styled.div`
+    display: flex;
+`
