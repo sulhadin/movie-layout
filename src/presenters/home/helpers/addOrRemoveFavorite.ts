be0766b57b543afd48ebadb7641e94451989cdd4
sulhadin <sulhadin@gmail.com>
2021-03-15 23:07:52 +0300

    refactor: Refactored adOrFavorite function.

--- src/presenters/home/helpers/addOrRemoveFavorite.ts
+++ src/presenters/home/helpers/addOrRemoveFavorite.ts
@@ -1,11 +1,7 @@
 export default function addOrRemoveFavorite(favorites: string[], uId: string): string[] {
-  let newFavorites = [...favorites]
-
   if (favorites.includes(uId)) {
-    newFavorites = [...favorites.filter((movieId) => movieId !== uId)]
-  } else {
-    newFavorites.push(uId)
+    return favorites.filter((movieId) => movieId !== uId)
   }
 
-  return newFavorites
+  return [...favorites, uId]
 }
